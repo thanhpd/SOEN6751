7c300698461a11eb36dc26a58d51e0e5084ff20d
Mohamad Salim <mohammedsalim1909@hotmail.com>
2025-02-25 16:21:04 -0500

    added config files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,8 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+import '../../global.css';
+
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
