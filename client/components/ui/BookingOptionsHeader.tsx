ed0a8694a1a1a0d5529e07c12d4ede8f37870153
Omnia Alam <omniaalam@yahoo.com>
2025-03-19 02:03:08 -0400

    Cancel booking and add event to calendar

--- client/components/ui/BookingOptionsHeader.tsx
+++ client/components/ui/BookingOptionsHeader.tsx
@@ -5,13 +5,8 @@ import { Text } from 'react-native-paper'
 const BookingOptionsHeader = () => {
     return (
         <View className="p-2">
-            <Text className="center" variant="titleMedium">
-                Lock in Your
-            </Text>
-            <Text variant="titleLarge">Next Fitness Move</Text>
-            <Text variant="titleSmall">
-                Choose from in-person & online activities, personal training, or
-                nutrition sessions.
+            <Text variant="titleLarge" style={{ textAlign: 'center' }}>
+                Next Fitness Move
             </Text>
         </View>
     )
