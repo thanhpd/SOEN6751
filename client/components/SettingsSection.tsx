61ecd6b438494d13c939b09654eed85c5161687d
Mohamad Salim <mohammedsalim1909@hotmail.com>
2025-03-19 00:14:32 -0400

    added the more section in settings/profile page

--- client/components/SettingsSection.tsx
+++ client/components/SettingsSection.tsx
@@ -24,7 +24,7 @@ const SettingsSection: React.FC = () => {
             </View>
 
             <View className="px-4 pb-4">
-                <View className="flex-row items-center justify-between mb-2">
+                <View className="flex-row items-center justify-between mb-1">
                     <View className="flex-row items-center space-x-3">
                         <View className="w-10 h-10 items-center justify-center">
                             <Image
@@ -51,7 +51,7 @@ const SettingsSection: React.FC = () => {
                     />
                 </View>
 
-                <View className="flex-row items-center justify-between">
+                <View className="flex-row items-center justify-between mb-1">
                     <View className="flex-row items-center space-x-3">
                         <View className="w-10 h-10 items-center justify-center">
                             <Image
@@ -79,7 +79,7 @@ const SettingsSection: React.FC = () => {
                 </View>
 
                 <View
-                    className="flex-row items-center justify-between"
+                    className="flex-row items-center justify-between mb-1"
                     onTouchEnd={() => console.log("payment pressed")}
                 >
                     <View className="flex-row items-center space-x-3">
@@ -95,7 +95,131 @@ const SettingsSection: React.FC = () => {
                     </View>
                     <Image
                         source={require('../assets/images/chevron.forward.svg')}
-                        style={{ width: 7, height: 12 }}
+                        style={{ width: 7, height: 12, marginRight: 10 }}
+                    />
+                </View>
+
+                <View className="p-1 mt-2">
+                    <Text className="text-lg font-semibold">More</Text>
+                </View>
+
+                <View
+                    className="flex-row items-center justify-between mb-1"
+                    onTouchEnd={() => console.log("faq pressed")}
+                >
+                    <View className="flex-row items-center space-x-3">
+                        <View className="w-10 h-10 items-center justify-center">
+                            <Image
+                                source={require('../assets/images/faq-icon.svg')}
+                                style={{ width: 24, height: 24 }}
+                            />
+                        </View>
+                        <Text className="text-base font-medium ml-2">
+                            FAQ
+                        </Text>
+                    </View>
+                    <Image
+                        source={require('../assets/images/chevron.forward.svg')}
+                        style={{ width: 7, height: 12, marginRight: 10 }}
+                    />
+                </View>
+
+                {/* Horizontal line divider within the same View */}
+                <View className="flex-row items-center justify-between mb-2">
+                    <View
+                        style={{
+                            flex: 1,
+                            height: 1,
+                            backgroundColor: '#EDEFF1',
+                            marginLeft: 40
+                        }}
+                    />
+                </View>
+
+                <View
+                    className="flex-row items-center justify-between mb-1"
+                    onTouchEnd={() => console.log("t&c pressed")}
+                >
+                    <View className="flex-row items-center space-x-3">
+                        <View className="w-10 h-10 items-center justify-center">
+                            <Image
+                                source={require('../assets/images/terms-conditions-icon.svg')}
+                                style={{ width: 24, height: 24 }}
+                            />
+                        </View>
+                        <Text className="text-base font-medium ml-2">
+                            Terms & Conditions
+                        </Text>
+                    </View>
+                    <Image
+                        source={require('../assets/images/chevron.forward.svg')}
+                        style={{ width: 7, height: 12, marginRight: 10 }}
+                    />
+                </View>
+
+                {/* Horizontal line divider within the same View */}
+                <View className="flex-row items-center justify-between mb-2">
+                    <View
+                        style={{
+                            flex: 1,
+                            height: 1,
+                            backgroundColor: '#EDEFF1',
+                            marginLeft: 40
+                        }}
+                    />
+                </View>
+
+                <View
+                    className="flex-row items-center justify-between mb-1"
+                    onTouchEnd={() => console.log("contact us pressed")}
+                >
+                    <View className="flex-row items-center space-x-3">
+                        <View className="w-10 h-10 items-center justify-center">
+                            <Image
+                                source={require('../assets/images/contact-us-icon.svg')}
+                                style={{ width: 24, height: 24 }}
+                            />
+                        </View>
+                        <Text className="text-base font-medium ml-2">
+                            Contact Us
+                        </Text>
+                    </View>
+                    <Image
+                        source={require('../assets/images/chevron.forward.svg')}
+                        style={{ width: 7, height: 12, marginRight: 10 }}
+                    />
+                </View>
+
+                {/* Horizontal line divider within the same View */}
+                <View className="flex-row items-center justify-between mb-2">
+                    <View
+                        style={{
+                            flex: 1,
+                            height: 1,
+                            backgroundColor: '#EDEFF1',
+                            marginLeft: 40
+                        }}
+                    />
+                </View>
+
+                <View
+                    className="flex-row items-center justify-between"
+                    onTouchEnd={() => console.log("log out pressed")}
+                >
+                    <View className="flex-row items-center space-x-3">
+                        <View className="w-10 h-10 items-center justify-center">
+                            <Image
+                                source={require('../assets/images/log-out-icon.svg')}
+                                style={{ width: 24, height: 24 }}
+                            />
+                        </View>
+                        <Text className="text-base font-medium ml-2">
+                            Log Out
+                        </Text>
+                    </View>
+                    <Image
+                        source={require('../assets/images/chevron.forward.svg')}
+                        style={{ width: 7, height: 12, marginRight: 10 }}
                     />
                 </View>
             </View>
