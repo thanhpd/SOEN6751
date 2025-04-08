3e588727e4582a1178c348d899abf3b1ff3f8337
Omnia Alam <badprogrammer008@gmail.com>
2025-04-08 18:20:35 -0400

    padding nutrition

--- client/app/nutrition.tsx
+++ client/app/nutrition.tsx
@@ -281,7 +281,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginTop: 10,
         position : 'absolute',
-        bottom : 150,
+        bottom : 90,
         left : 175,
     },
     dot: {
